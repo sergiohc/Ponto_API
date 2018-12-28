73bd630e0ec4c7ed9f8d43f2d70710edaeb8b8a4
Sergio <sergioh.c@live.com>
2018-12-28 04:52:46 -0200

    filter for show clock

--- app/controllers/api/v1/clock_in_out_controller.rb
+++ app/controllers/api/v1/clock_in_out_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::ClockInOutController < Api::V1::ApiController
-  #before_action :set_employee_clock, only: [:show, :update, :destroy]
-  #before_action :set_employ
+  #before_action :authenticate_api_v1_user!, except: [:show]
+  #before_action :set_employee
+  before_action :set_clock_employee, only: [:update, :destroy, :show]
+  #before_action :allow_only_owner, only: [:create, :update, :destroy]
+  
   def index
-    #@clock = ClockInOut.find(:all,:order=> 'date desc')
-    @clock = ClockInOut.all
+    @clock = @employee.clock_in_out
     render json: @clock
   end
 
@@ -27,16 +29,26 @@ class Api::V1::ClockInOutController < Api::V1::ApiController
   end
 
   private
-  def set_employ
-    @employee = (@employee_clock)? @employee_clock.employee :  Employee.find(params[:employee_id])  
+  def set_employee
+    @employee =  Employee.find(params[:employee_id])  
   end
 
-  def set_employee_clock
-    @employee_clock = ClockInOut.find(params[:employee_id])    
+  def set_clock_employee
+    if (params[:day].present?)
+      @employee_clock = ClockInOut.where(employee_id: params[:id], date: Date.today)
+    else
+      @employee_clock = ClockInOut.where(employee_id: params[:id])
+    end
   end
 
   def clock_params
-    params.require(:clock_in_out).permit(:date, :h1, :h2, :h3, :h4)
+    params.require(:clock_in_out).permit(:date, :h1, :h2, :h3, :h4, :day)
   end 
+
+  def allow_only_owner
+    unless current_api_v1_user == @employee.user
+      render(json: {}, status: :forbidden) and return
+    end
+  end
 
 end
--- app/models/clock_in_out.rb
+++ app/models/clock_in_out.rb
@@ -1,6 +1,4 @@
 class ClockInOut < ApplicationRecord
-  extend FriendlyId
-  friendly_id :employee_id, use: :slugged
   belongs_to :employee
-  #validates_uniqueness_of :date, :scope => "employee_id"
+  scope :day, -> { where(status: true) }
 end
