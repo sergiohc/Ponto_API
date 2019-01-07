7bc585967097a5eb2da54cbc9a17ea93e0c5c279
Sergio <sergioh.c@live.com>
2019-01-07 05:09:58 -0200

    remoce seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Employee.create(name: 'Sergio Henrique',
-  cpf: '07664434939',
-  pis: '23212345674',
-  user: User.last)
 
-Employee.create(name: 'Leonardo Mark',
-  cpf: '11114434930',
-  pis: '13232345672',
-  user_id: 1)
-
-ClockInOut.create( date: Date.current,
-  hour1: Time.now,
-  hour2: Time.now,
-  hour3: Time.now,
-  hour4: Time.now,
-  employee: Employee.last
-)
 
